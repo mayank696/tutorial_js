c84965800947e1afd03ac92ed36922c0de43afe6
mayank696 <mayank696@gmail.com>
2014-09-30 17:44:40 +0530

    Modification in App

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-/*App Js*/
+/* Function for clock */
+
 var start = setInterval(function() {myClock()}, 500);
 
-function myClock(){
+ function myClock(){
     var clock = new Date();
     var h = clock.getHours();
     var m = clock.getMinutes();
     var s = clock.getSeconds();
-
     document.getElementById("demo").innerHTML = h + ":" + m + ":" + s;
 }
 
+/* Function to pop up alert, when user enters his name */
 
 function myFunThanks(){
     alert(" Thanks & Your Welcome!");
 }
 
+/* Function to change the background color of page whene a user hovers over the table's columns */
 
 function changeBg(color){
     document.body.style.backgroundColor = color;
